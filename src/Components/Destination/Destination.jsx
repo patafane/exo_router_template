93151438e54695dbff4eb50268a2f909ff9781d9
unknown <cecilienhelfter@gmail.com>
2024-02-29 17:01:32 +0100

    fin de journée

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -1,9 +1,15 @@
 import Navbar from "../Navbar/Navbar"
+import Data from "./../../data/data.json"
 import "./_Destination.sass"
 let Destination = ()=>{
     return(
         <div className="destination">
             <Navbar/>
+            {Data.destinations.map((element,index)=>(
+                <div className="planet">
+                    {element.name}
+                </div>
+            ))}
         </div>
     )
 }
